6e08187693e92e835e3b23b6facdf6738816b5aa
myguddy <seungil.han@gmail.com>
2019-10-25 09:57:13 +0900

    fix jdbc compile error

--- src/main/java/io/noga/sample/SampleApplication.java
+++ src/main/java/io/noga/sample/SampleApplication.java
@@ -3,7 +3,9 @@ package io.noga.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SampleApplication {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SampleApplication.class);
